734e83d969d789001bb36bbdfb817d0d7a3da7e9
arunclenten-1 <arunclenten1234@gmail.com>
2024-06-10 18:44:33 +0530

    update the new code

--- app.js
+++ app.js
@@ -11,3 +11,40 @@
     //     document.body.classList.toggle("light-mode");
     // })
 })();
+
+
+(function () {
+    emailjs.init("QdV7ifPIcdSwZm0sj");
+  })();
+
+  function sendemail(event) {
+    event.preventDefault(); // Prevent the form from submitting in the default way
+
+    var email = document.getElementById("email").value;
+    var name = document.getElementById("name").value;
+    var message = document.getElementById("message").value;
+    var subject = document.getElementById("subject").value;
+    var number = document.getElementById("number").value;
+
+    var templateParams = {
+      email: email,
+      to_name: name,
+      message: message,
+      subject: subject,
+      number: number,
+    };
+
+    emailjs
+      .send("service_3kc2hco", "template_xrnvt8t", templateParams)
+      .then(function (response) {
+        console.log("SUCCESS!", response.status, response.text);
+        window.alert("Sent successfully!");
+
+        // Reset the form fields
+        document.getElementById("contactForm").reset();
+      })
+      .catch(function (error) {
+        console.log("FAILED...", error);
+        window.alert("Failed to send email. Please try again.");
+      });
+  }
